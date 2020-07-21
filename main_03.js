f5a7dec503aefeb979c7a970c75db2fad1b3b512
macbook <jackiepro23@gmail.com>
2020-07-21 23:09:08 +0300

    module 3

---
+++ main_03.js
@@ -0,0 +1,54 @@
+//Задание 3 - done
+//Напиши функцию findBestEmployee(employees), 
+//которая принимает объект сотрудников 
+//и возвращает имя самого продуктивного (который выполнил больше всех задач). Сотрудники и кол-во выполненых задач содержатся как свойства объекта в формате "имя":"кол-во задач".
+
+//first option
+const findBestEmployee = function(employees) {
+  //твой код
+  const keys = Object.keys(employees);
+  const value = Object.values(employees);
+
+  let biggestValue = value[0];
+  let bestEmployer = keys[0]
+
+  for (let i = 1; i < value.length; i += 1) {
+    if(value[i] > biggestValue) {
+        biggestValue = value[1];
+        bestEmployer = keys[1]
+    }
+
+    return bestEmployer;
+  }
+};
+
+
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
